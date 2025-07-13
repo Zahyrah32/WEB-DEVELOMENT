53d05eadfa6aca7c778e5b4a4b40f9790d3970df
Zahyrah32 <zahirahahwan32@gmail.com>
2025-07-14 00:43:39 +0800

    Delete lab6_q3.php

--- lab6_q3.php
+++
@@ -1,9 +0,0 @@
-<?php
-function calculateArea($width, $height) {
-    return $width * $height;
-}
-$width = 4;
-$height = 2;
-$area = calculateArea($width, $height);
-echo "The area of a rectangle with width $width and height $height is $area";
-?>
